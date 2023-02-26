f099ff1c095ef48d6c2646b3ea034676d62e5d93
AlexDev404 <alexdev404@users.noreply.github.com>
2023-02-25 22:50:16 -0600

    Edit index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 </script>
 
 <main>
+<title>UBIT | 200</title>
 <h1 class="text-xl font-bold text-gray-900 px-4 py-2">UBIT</h1>
 <p class="text-sm font-light text-gray-500">Page deployed</p>
 </main>
