1428f1be456563575f32c631e1e73d3fb9b1d4a2
AlexDev404 <alexdev404@users.noreply.github.com>
2023-02-25 21:01:31 -0600

    Finish setup
    Co-authored-by: Abel Blanco <Abelradblanco@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { version } from './package.json';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	define: {
+		'process.env': {},
+		'process.platform': false,
+		__VERSION__: version
+	},
+	resolve: {
+		alias: {
+			emitter: 'emitter-component',
+			Buffer: 'buffer/'
+		}
+	}
 });
